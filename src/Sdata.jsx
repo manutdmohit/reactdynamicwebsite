f7662fee5b4c18bc7b661a2a67af6755d836950c
Mohit Saud <saudmohit@gmail.com>
2021-03-26 09:37:21 +0545

    Initial Commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,30 @@
+import web from "../src/images/2.svg";
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title: "Web Development",
+  },
+  {
+    imgsrc: web,
+    title: "Banking",
+  },
+  {
+    imgsrc: web,
+    title: "Web Development",
+  },
+  {
+    imgsrc: web,
+    title: "Web Development",
+  },
+  {
+    imgsrc: web,
+    title: "Web Development",
+  },
+  {
+    imgsrc: web,
+    title: "Web Development",
+  },
+];
+
+export default Sdata;
